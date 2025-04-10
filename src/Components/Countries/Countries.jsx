df62bd5b732bc9c9a1a54c3198a7b36b21f45501
Nayeem <mahfujurrahamannayeem@gmail.com>
2025-04-10 11:59:46 +0600

    Toggoling Starts

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
 const Countries = () => {
-    const [countries,setCountries] = useState([]);
+    const [countries, setCountries] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setCountries(data));
+            .then(res => res.json())
+            .then(data => setCountries(data));
 
 
-    },[])
+    }, [])
 
     return (
-        <div>
-            <h3>Countries : {countries.length}</h3>
+
+        
+            <div>
+                <h3>Countries : {countries.length}</h3>
+                <div className='container'>
             {
                 countries.map(country => <Country key={country.cca3} country={country}></Country>)
             }
         </div>
+            </div>
+           
     );
 };
 
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
 import './country.css'
 const Country = ({country}) => {
     console.log(country)
-    const {name,flags,population} = country;
+    const {name,flags,population,area,cca3} = country;
+    const [visited,setVisited]=useState(false);
+    const handleVisited = () =>{
+        setVisited(true);
+    }
     return (
         <div className='country'>
             <h2>Name : {name.common}</h2>
-            <img src={flags.png} alt="" />
+            <img className='photo' src={flags.png} alt="" />
             <h2>Population : {population}</h2>
+            <h2>Area : {area}</h2>
+            <h2>Code : {cca3}</h2>
+            <button onClick={handleVisited}>Visited</button>
+            {visited ? 'I Have Visited This Country' : 'I Want To Visit This Contry'}
         </div>
     );
 };
